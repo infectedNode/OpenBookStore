bfa9df323eecbad17a1e3d84396ee4e26a75acf4
Shivam Sharma <shivam231198@gmail.com>
2020-01-06 14:01:11 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi from Server');
+});
+
+const port = process.env.PORT || 2000;
+
+app.listen(port, () => {
+    console.log(`Server started successfully on port ${port}`);
+})
